c35e492d93ca089c291ccb838c8d6accad3e56fd
tarunmadasu <46481446+tarunmadasu@users.noreply.github.com>
2019-01-09 16:21:33 +0530

    arrays

---
+++ ReverseArray/src/com/app/Reverse.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class Reverse {
+ public static void main(String[] args) {
+	
+	 int[] a={1,2,3,4,5};
+	 for(int i=a.length-1;i>=0;i--){
+		 System.out.println(a[i]);
+	 }
+}
+}
